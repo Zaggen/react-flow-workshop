06ca9bf8931f90b687be3e66dd1b9e9fa5981c65
Felipe Roman <xpynesoul@gmail.com>
2018-08-15 00:53:47 +0200

    Fixes types

--- src/exercises/2-read-only/Films.js
+++ src/exercises/2-read-only/Films.js
@@ -1,15 +1,16 @@
 // @flow
 // TODO: Fix types
 import React from 'react'
-import AverageRank from './AverageRank'
+import AverageRating from './AverageRating'
+import type { List, Map } from './types'
 
-type Props = {|
-  films: Array<{ id: number, name: string, rank: number }>,
-|}
+type Props = Map<{|
+  films: List<{ id: number, name: string, rating: number }>,
+|}>
 
 const Films = ({ films }: Props) => (
   <div>
-    <AverageRank films={films} />
+    <AverageRating films={films} />
     <ul>{films.map((film, i) => <li key={i}>{film.name}</li>)}</ul>
   </div>
 )
