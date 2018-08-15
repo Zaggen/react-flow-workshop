171039adaf1dd0e41c597284bd0f7ce375eb6456
Felipe Roman <xpynesoul@gmail.com>
2018-08-15 14:50:14 +0200

    Initial commit

---
+++ src/exercises/7-rematch-models/allModels.types.js
@@ -0,0 +1,13 @@
+// @flow
+import type { GetDispatch, Model } from './helperTypes'
+// TODO: Import types
+
+export type Dispatch = GetDispatch<{}>
+
+// TODO: Add all model's state
+export type State = {}
+
+// export type CountModel = Model<CountState, CountActions, Dispatch, State>
+
+// TODO: All add Models here
+export type Models = {}
