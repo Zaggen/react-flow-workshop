c4ebe6f59bd6ea0a33be3ea5a16be971e75c8d7a
Felipe Roman <xpynesoul@gmail.com>
2018-08-15 14:35:13 +0200

    Imports CountModel and removes explicit types

--- src/exercises/7-rematch-models/Model.js
+++ src/exercises/7-rematch-models/Model.js
@@ -1,23 +1,26 @@
 // @flow
+import type { CountModel } from './allModels.types'
 type State = number
 
-export const count = {
+export const count: CountModel = {
   state: 0, // initial state
   reducers: {
     // handle state changes with pure functions
-    increment(state: State) {
-      return (payload: number): State => state + payload
+    increment(state, payload) {
+      return state + payload
     },
-    multiply(state: State) {
-      return (payload: Object): State => state + payload.x
+    multiply(state, payload) {
+      return state + payload.x
     },
   },
-  effects: (dispatch: any) => ({
+  effects: dispatch => ({
     // handle state changes with impure functions.
     // use async/await for async actions
-    async incrementAsync(payload: Object) {
+    async incrementAsync(payload, rootState) {
+      const user = rootState.user[0]
       await new Promise(resolve => setTimeout(resolve, 1000))
-      dispatch.increment(payload)
+      dispatch.count.increment(payload)
+      dispatch.user.createAsync(payload)
     },
   }),
 }
