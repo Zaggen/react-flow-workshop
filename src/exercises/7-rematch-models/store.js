ee3aa6b716241535656c11afd40c2df16e015965
Felipe Roman <xpynesoul@gmail.com>
2018-08-15 14:34:10 +0200

    Removes type related code

--- src/exercises/7-rematch-models/store.js
+++ src/exercises/7-rematch-models/store.js
@@ -1,9 +1,4 @@
 // @flow
 import count from './Model'
-import type { GetDispatch, GetState } from './types'
 
 const models = { count }
-
-export type State = GetState<typeof models>
-
-export type Dispatch = GetDispatch<typeof models>
