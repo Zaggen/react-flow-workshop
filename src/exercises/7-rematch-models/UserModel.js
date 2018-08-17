cddf748cf721bac85d4b7ebef7c56b7f1d7c0eb6
Felipe Roman <xpynesoul@gmail.com>
2018-08-17 22:29:22 +0200

    Initial commit

---
+++ src/exercises/7-rematch-models/UserModel.js
@@ -0,0 +1,21 @@
+// @flow
+import type { Model } from './allModels.types'
+
+export const user: Model<'user'> = {
+  state: [{ name: 'Lu' }], // initial state
+  reducers: {
+    add(state, payload) {
+      return state
+    },
+    remove(state, payload) {
+      return state
+    },
+  },
+  effects: dispatch => ({
+    async createAsync(payload, rootState) {
+      //await dispatch.count.incrementAsync()
+    },
+  }),
+}
+
+export default user
