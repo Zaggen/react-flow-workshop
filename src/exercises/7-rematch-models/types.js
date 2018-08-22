253b1dad0282882b12efaa2c1fef7a31b2996218
Felipe Roman <xpynesoul@gmail.com>
2018-08-22 14:30:52 +0200

    Refactor example to use define an interface

--- src/exercises/7-rematch-models/types.js
+++ src/exercises/7-rematch-models/types.js
@@ -1,19 +1,18 @@
 // @flow
 
-// TODO: Define CountState here
-
-// TODO: Define CountActions here
+// TODO: Define CountInterface here
 
 // Example
 
-export type UsersState = { name: string }[]
-
-export type UserActions = {
-  pure: {
-    add: { payload: { name: string } },
-    remove: { payload: { id: number } },
-  },
-  effects: {
-    createAsync: { payload: { name: string } },
+export type UserInterface = {
+  state: {| +name: string |}[],
+  actions: {
+    pure: {
+      add: { name: string },
+      remove: { id: number },
+    },
+    effects: {
+      createAsync: { name: string },
+    },
   },
 }
