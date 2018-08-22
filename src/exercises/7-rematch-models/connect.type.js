e8055aac8ae001a3e64670cb8947b8f3fc0e1db6
Felipe Roman <xpynesoul@gmail.com>
2018-08-22 14:13:32 +0200

    Wraps Wrapped component props in ReadOnly

--- src/exercises/7-rematch-models/connect.type.js
+++ src/exercises/7-rematch-models/connect.type.js
@@ -6,5 +6,5 @@ export type Connect = <MS, MP, R1, R2>(
   mapState: (state: $ReadOnly<State>) => R1,
   mapDispatch: (state: $ReadOnly<Dispatch>) => R2,
 ) => (
-  React.ComponentType<{ ...$Exact<R1>, ...$Exact<R2> }>,
+  React.ComponentType<$ReadOnly<{ ...$Exact<R1>, ...$Exact<R2> }>>,
 ) => React.ComponentType<{}>
