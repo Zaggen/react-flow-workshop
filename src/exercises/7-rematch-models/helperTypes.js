0fb7db1a4fbfd835029a2a0a488cb3bc6c5ca2e5
Felipe Roman <xpynesoul@gmail.com>
2018-08-22 14:20:57 +0200

    Improves helpers

--- src/exercises/7-rematch-models/helperTypes.js
+++ src/exercises/7-rematch-models/helperTypes.js
@@ -1,39 +1,67 @@
 // @flow
-export type GetDispatch<Actions> = $ObjMap<
-  Actions,
-  <A: { pure: Object, effects: Object }>(
-    action: A,
-  ) => {|
-    ...$Exact<
-      $ObjMap<
-        $ElementType<A, 'pure'>,
-        <V>(val: V) => ($ElementType<V, 'payload'>) => void,
-      >,
-    >,
-    ...$Exact<
-      $ObjMap<
-        $ElementType<A, 'effects'>,
-        <V>(val: V) => ($ElementType<V, 'payload'>) => void,
-      >,
-    >,
-  |},
+type GetActions<Interfaces> = $ReadOnly<
+  $ObjMap<
+    Interfaces,
+    <I: { state: any, actions: Object }>(I) => $PropertyType<I, 'actions'>,
+  >,
 >
 
-export type Model<S, A, D, RootState> = {
-  state: S,
-  reducers: $ObjMap<
-    $ElementType<A, 'pure'>,
-    <V>(val: V) => (state: S, payload: $ElementType<V, 'payload'>) => S,
+type GetStates<Interfaces> = $ReadOnly<
+  $ObjMap<
+    Interfaces,
+    <I: { state: any, actions: Object }>(I) => $PropertyType<I, 'state'>,
   >,
-  effects: (
-    dispatch: D,
+>
+
+type Return<T> = () => T
+type Fn<A> = A => void
+
+export type GetDispatch<Interfaces> = $ReadOnly<
+  $ObjMap<
+    GetActions<Interfaces>,
+    <A: { pure: Object, effects: Object }>(
+      action: A,
+    ) => {|
+      ...$Exact<
+        $ObjMap<$PropertyType<A, 'pure'>, <V>(V) => Fn<$Call<Return<V>>>>,
+      >,
+      ...$Exact<
+        $ObjMap<$PropertyType<A, 'effects'>, <V>(V) => Fn<$Call<Return<V>>>>,
+      >,
+    |},
+  >,
+>
+
+export type GetState<Interfaces> = $ReadOnly<
+  $ObjMap<GetStates<Interfaces>, <S>(S) => S>,
+>
+
+type Model<S, A, D, RootState> = {
+  +state: $Call<Return<S>>,
+  +reducers: $ObjMap<
+    $ElementType<A, 'pure'>,
+    <V>(val: V) => (state: S, payload: V) => $Call<Return<S>>,
+  >,
+  +effects: (
+    dispatch: $ReadOnly<D>,
   ) => $ObjMap<
     $ElementType<A, 'effects'>,
     <V>(
       val: V,
-    ) => (
-      payload: $ElementType<V, 'payload'>,
-      rootState: RootState,
-    ) => Promise<void>,
+    ) => (payload: V, rootState: $ReadOnly<RootState>) => Promise<void>,
   >,
 }
+
+export type GetModels<Interfaces, Dispatch, RootState> = $Exact<
+  $ObjMap<
+    Interfaces,
+    <I: { state: any, actions: Object }>(
+      I,
+    ) => Model<
+      $PropertyType<I, 'state'>,
+      $PropertyType<I, 'actions'>,
+      Dispatch,
+      RootState,
+    >,
+  >,
+>
